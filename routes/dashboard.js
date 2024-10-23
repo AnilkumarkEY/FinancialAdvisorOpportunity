5707a02f9dc90fce3adcbc67f64a0993bef4878b
rubiiEy <rubi.devi@in.ey.com>
2024-10-23 16:44:00 +0530

    auth changes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,12 @@
 const { dashboardController } = require('../controllers');
+const {eventValidation} = require("../middleware/eventValidation");
+const {authentication} = require("../middleware/authentication")
 
 async function dashboardRoutes(fastify, options) {
     // Define dashboard routes
-    fastify.get('/dashboard/leadTypeOverview', dashboardController.getLeadCountByType);
-    fastify.get('/dashboard/leadStatusOverview', dashboardController.getLeadStatusCount);
-    fastify.post('/dashboard/leadStatusWithPagination', dashboardController.getLeadsByStatusWithPagination);
+    fastify.get('/dashboard/leadTypeOverview', { preHandler: [authentication, eventValidation] }, dashboardController.getLeadCountByType);
+    fastify.get('/dashboard/leadStatusOverview', { preHandler: [authentication, eventValidation] }, dashboardController.getLeadStatusCount);
+    fastify.post('/dashboard/leadStatusWithPagination', { preHandler: [authentication, eventValidation] }, dashboardController.getLeadsByStatusWithPagination);
 }
 
 module.exports = dashboardRoutes;

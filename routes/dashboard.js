1bc8ffba205dfc7f3e4abfcaf5bd1093c463c6c2
AnilkumarkEY <k.kumar3@in.ey.com>
2024-10-23 15:43:10 +0530

    resolved conflicts

---
+++ routes/dashboard.js
@@ -0,0 +1,19 @@
+const { dashboardController } = require("../controllers");
+
+async function dashboardRoutes(fastify, options) {
+  // Define dashboard routes
+  fastify.get(
+    "/dashboard/leadTypeOverview",
+    dashboardController.getLeadCountByType
+  );
+  fastify.get(
+    "/dashboard/leadStatusOverview",
+    dashboardController.getLeadStatusCount
+  );
+  fastify.post(
+    "/dashboard/leadStatusWithPagination",
+    dashboardController.getLeadsByStatusWithPagination
+  );
+}
+
+module.exports = dashboardRoutes;
